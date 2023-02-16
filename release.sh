399fb9f152b99ca247ba0501e1148fde9b8c7a2f
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-02-15 21:05:16 -0600

    i used the github editor, im not sorry
    call the cops! the first script i pushed was the wrong version and only compiled the systems i was testing, instead of all combos as intended.

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # MIT License 
 # donuts-are-good
 
-# Check for the --help flag
+# check for the --help flag
 if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
     # Display the man page
     man ./doc/release.sh.1
@@ -11,41 +11,68 @@ if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
     exit 0
 fi
 
-# Parse the command line arguments
-while [[ $# -gt 0 ]]
-do
-    # Set the key variable to the first argument
-    key="$1"
-
-    # Check the value of the key variable
-    case $key in
-        # If the key is "--name", set the BINARY_NAME variable to the second argument
-        --name)
-        BINARY_NAME="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        # If the key is "--version", set the VERSION variable to the second argument
-        --version)
-        VERSION="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        # If the key is unknown, skip it
-        *)    # unknown option
-        shift # past argument
-        ;;
-    esac
+# get command line args
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -n|--name)
+      name="$2"
+      shift 2
+      ;;
+    -v|--version)
+      version="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
 done
 
-# Build for Windows on AMD64
-GOOS=windows GOARCH=amd64 go build -o "${BINARY_NAME}-${VERSION}-windows-amd64.exe"
+if [ -z "$name" ] || [ -z "$version" ]; then
+  echo "Usage: $0 --name NAME --version VERSION"
+  exit 1
+fi
 
-# Build for macOS on Intel
-GOOS=darwin GOARCH=amd64 go build -o "${BINARY_NAME}-${VERSION}-darwin-amd64"
+# make the build folder
+output_dir="BUILDS"
+mkdir -p $output_dir
 
-# Build for macOS on Apple Silicon
-GOOS=darwin GOARCH=arm64 go build -o "${BINARY_NAME}-${VERSION}-darwin-arm64"
+# get the list of all build targets
+os_list=$(go tool dist list | cut -f1 -d'/')
+arch_list=$(go tool dist list | cut -f2 -d'/')
 
-# Build for Linux on AMD64
-GOOS=linux GOARCH=amd64 go build -o "${BINARY_NAME}-${VERSION}-linux-amd64"
+# split the os and arch lists into arrays
+IFS=$'\n' read -d '' -r -a os_array <<<"$os_list"
+IFS=$'\n' read -d '' -r -a arch_array <<<"$arch_list"
+
+# loop through and build all possible types
+for os in "${os_array[@]}"; do
+  for arch in "${arch_array[@]}"; do
+    
+    # set the scene for the current build
+    export GOOS=$os
+    export GOARCH=$arch
+
+    # build and check
+    binary_name="$name-$version-$GOOS-$GOARCH"
+    if [[ "$GOOS" == "windows" ]]; then
+      binary_name="$binary_name.exe"
+    fi
+    build_dir="$output_dir/$GOOS/$GOARCH"
+    mkdir -p "$build_dir"
+    output_file="$build_dir/$binary_name"
+    if go build -o "$output_file" . > /dev/null 2>&1; then
+      echo "pass - $GOOS - $GOARCH"
+    else
+      echo "fail - $GOOS - $GOARCH"
+      rm -f "$output_file"
+    fi
+
+    # cleanup for next cycle
+    # todo check if this is unsetting systemwide
+    unset GOOS
+    unset GOARCH
+  done
+done
